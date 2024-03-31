5e5fc880362a3b06468dc23bde410bcbc7ed8c61
Mayuri-Katkar <152522839+Mayuri-pr@users.noreply.github.com>
2024-03-31 17:14:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -27,7 +28,7 @@ const __dirname= path.dirname(__filename);
 
 
 // rest object
-const express = require('express');
+
 const app = express();
 
 //middlewares
