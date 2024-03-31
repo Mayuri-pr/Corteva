9a9deb6256cf61a047621c3c1eb391f4f1d62f8b
Mayuri-Katkar <152522839+Mayuri-pr@users.noreply.github.com>
2024-03-31 15:46:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from "cors";
 import path from 'path'
+import { fileURLToPath } from "url";
 
 
 
@@ -17,6 +18,14 @@ dotenv.config();
 // database config
 
 connectDB();
+
+
+// ES Module Fix
+
+const __filename= fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
+
+
 
 // rest object
 
