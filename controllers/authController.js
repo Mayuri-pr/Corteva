7760bc750359d84e194440cbf53352afbb3ace4f
Mayuri-Katkar <152522839+Mayuri-pr@users.noreply.github.com>
2024-04-01 22:54:09 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-import userModel from "../models/userModel.js";
+const userModel = require('../models/userModel');
+
 import orderModel from "../models/orderModel.js";
 import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
